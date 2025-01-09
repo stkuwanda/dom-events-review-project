24500d24189dafe6baa60ab4aa647dbd8f3d3e89
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-09 10:10:03 +0200

    attaching event handler functions to DOM element event properties

---
+++ EventBasics/app.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('v2');
+
+function run() {
+    console.dir(button);
+}
+
+button.onclick = function () {
+    console.log('I got clicked!');
+}
+
+// information on events found on an element can be found in the DOM Web API.
+// https://developer.mozilla.org/en-US/docs/Web/API/Element
